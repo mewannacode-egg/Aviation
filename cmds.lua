435989e1112b2b34dcca93ff766f8b39582523a9
mewannacode-egg <mewannacode@gmail.com>
2026-03-25 22:43:14 +0530

    Update cmds.lua

--- cmds.lua
+++ cmds.lua
@@ -1,1 +1,9 @@
+local funcs = loadstring(game:HttpGet("https://raw.githubusercontent.com/<username>/<repo>/main/func.lua"))()
+local commands = {}
+local function addCommand(name, callback)
+    commands[name] = callback
+end
 
+
+
+return commands
