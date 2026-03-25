bfaefabda165deead7b9fbacd6aa23e9a5f0afdb
mewannacode-egg <mewannacode@gmail.com>
2026-03-25 22:42:00 +0530

    Update func.lua

--- func.lua
+++ func.lua
@@ -1,1 +1,15 @@
+local funcs = {}
+local Players = game:GetService("Players")
 
+function funcs.gotoPlayer(player, target)
+    if player and target and player.Character and target.Character then
+        local myRoot = player.Character:FindFirstChild("HumanoidRootPart")
+        local targetRoot = target.Character:FindFirstChild("HumanoidRootPart")
+
+        if myRoot and targetRoot then
+            myRoot.CFrame = targetRoot.CFrame + (targetRoot.CFrame.LookVector * -2)
+        end
+    end
+end
+
+return funcs
