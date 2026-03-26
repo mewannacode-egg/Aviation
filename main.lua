9b314f2783ac1ce5280809cf7f4d817d57d26f7e
mewannacode-egg <mewannacode@gmail.com>
2026-03-26 20:43:09 +0530

    Improve HTTP requests and player chat handling
    Refactor HTTP request handling and update player chat event connections.

--- main.lua
+++ main.lua
@@ -1,14 +1,36 @@
 local base = "https://raw.githubusercontent.com/mewannacode-egg/Aviation/main/"
 
-local funcs = loadstring(game:HttpGet(base .. "funcs.lua"))()
-local cmdsLoader = loadstring(game:HttpGet(base .. "cmds.lua"))()
+local requestFunc = (syn and syn.request)
+	or (http_request)
+	or (request)
+	or (fluxus and fluxus.request)
+	or function(tbl)
+		return {Body = game:HttpGet(tbl.Url)}
+	end
+
+local function get(url)
+	local res = requestFunc({
+		Url = url,
+		Method = "GET"
+	})
+	return res.Body
+end
+
+local funcs = loadstring(get(base .. "funcs.lua"))()
+local cmdsLoader = loadstring(get(base .. "cmds.lua"))()
 
 local cmds = cmdsLoader(funcs)
 
 local Players = game:GetService("Players")
 
-Players.PlayerAdded:Connect(function(player)
-	player.Chatted:Connect(function(msg)
-		cmds:Run(player, msg)
+for _, plr in ipairs(Players:GetPlayers()) do
+	plr.Chatted:Connect(function(msg)
+		cmds:Run(plr, msg)
+	end)
+end
+
+Players.PlayerAdded:Connect(function(plr)
+	plr.Chatted:Connect(function(msg)
+		cmds:Run(plr, msg)
 	end)
 end)
