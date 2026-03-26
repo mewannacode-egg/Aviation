3ea81e5641f72a5d95e6438da43ed86ef0d36355
mewannacode-egg <mewannacode@gmail.com>
2026-03-26 20:42:09 +0530

    Update funcs.lua

--- funcs.lua
+++ funcs.lua
@@ -5,19 +5,19 @@ local Players = game:GetService("Players")
 function funcs:GetPlayer(str)
 	str = string.lower(str)
 
-	for _, player in ipairs(Players:GetPlayers()) do
-		if string.find(string.lower(player.Name), str, 1, true)
-		or string.find(string.lower(player.DisplayName), str, 1, true) then
-			return player
+	for _, plr in ipairs(Players:GetPlayers()) do
+		if string.find(string.lower(plr.Name), str, 1, true)
+		or string.find(string.lower(plr.DisplayName), str, 1, true) then
+			return plr
 		end
 	end
 end
 
-function funcs:Goto(fromPlayer, toPlayer)
-	if not (fromPlayer.Character and toPlayer.Character) then return end
+function funcs:Goto(from, to)
+	if not (from.Character and to.Character) then return end
 
-	local hrp1 = fromPlayer.Character:FindFirstChild("HumanoidRootPart")
-	local hrp2 = toPlayer.Character:FindFirstChild("HumanoidRootPart")
+	local hrp1 = from.Character:FindFirstChild("HumanoidRootPart")
+	local hrp2 = to.Character:FindFirstChild("HumanoidRootPart")
 
 	if hrp1 and hrp2 then
 		hrp1.CFrame = hrp2.CFrame + Vector3.new(0, 3, 0)
