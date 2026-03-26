08fe9058ed3bd5115ec5d9facac548658c7e4ec9
mewannacode-egg <mewannacode@gmail.com>
2026-03-26 20:42:29 +0530

    Update cmds.lua

--- cmds.lua
+++ cmds.lua
@@ -3,7 +3,7 @@ return function(funcs)
 	local cmds = {}
 	cmds.Prefix = "-"
 
-	cmds.Commands = {
+	cmds.List = {
 
 		goto = {
 			Code = [[
@@ -15,32 +15,32 @@ return function(funcs)
 					funcs:Goto(player, target)
 				end
 			]]
-		}
+		},
 
 	}
 
-	function cmds:Run(player, message)
-		if message:sub(1,1) ~= self.Prefix then return end
+	function cmds:Run(player, msg)
+		if msg:sub(1,1) ~= self.Prefix then return end
 
 		local args = {}
-		for word in message:gmatch("%S+") do
+		for word in msg:gmatch("%S+") do
 			table.insert(args, word)
 		end
 
 		local cmdName = args[1]:sub(2):lower()
 		table.remove(args, 1)
 
-		local cmd = self.Commands[cmdName]
+		local cmd = self.List[cmdName]
 		if not cmd then return end
 
-		local source = [[
+		local src = [[
 			return function(player, args, funcs)
 		]] .. cmd.Code .. [[
 			end
 		]]
 
-		local func = loadstring(source)()
-		func(player, args, funcs)
+		local fn = loadstring(src)()
+		fn(player, args, funcs)
 	end
 
 	return cmds
