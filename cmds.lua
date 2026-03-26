ebe913e60c1b005c9c6f5d468bbbfe532d4aab1f
mewannacode-egg <mewannacode@gmail.com>
2026-03-26 18:58:29 +0530

    Update cmds.lua

--- cmds.lua
+++ cmds.lua
@@ -1,20 +1,49 @@
-local funcs = require(script.Parent.function)
+local funcs = require(script.Parent.funcs)
 
 local cmds = {}
 
-cmds["goto"] = function(args)
-    local targetName = args[1]
+cmds.Prefix = "-"
 
-    if not targetName then
-        warn("no target provided")
-        return
-    end
+cmds.List = {
 
-    local success, err = funcs.gotoPlayer(targetName)
+	goto = {
+		Description = "Teleport to a player",
+		
+		Run = function(player, args)
+			local targetName = args[1]
+			if not targetName then return end
 
-    if not success then
-        warn("[goto error]:", err)
-    end
+			local target = funcs:GetPlayerFromString(targetName)
+			if target then
+				funcs:GotoPlayer(player, target)
+			end
+		end
+	}
+
+}
+
+function cmds:Execute(player, message)
+	if string.sub(message, 1, 1) ~= self.Prefix then return end
+
+	local args = {}
+	for word in string.gmatch(message, "%S+") do
+		table.insert(args, word)
+	end
+
+	local commandName = string.sub(args[1], 2)
+	table.remove(args, 1)
+
+	local command = self.List[commandName]
+	if not command then return end
+
+	local code = [[
+		return function(player, args, command)
+			command.Run(player, args)
+		end
+	]]
+
+	local func = loadstring(code)()
+	func(player, args, command)
 end
 
 return cmds
