c3932377584040d6a5777f63667a979a401863ea
mewannacode-egg <mewannacode@gmail.com>
2026-03-26 19:12:15 +0530

    Update base URL for resource loading

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
-local base = "https://raw.githubusercontent.com/YOURNAME/YOURREPO/main/cmdsystem/"
+local base = "https://raw.githubusercontent.com/mewannacode-egg/main/main.lua/"
 
 local funcs = loadstring(game:HttpGet(base .. "funcs.lua"))()
 local cmdsLoader = loadstring(game:HttpGet(base .. "cmds.lua"))()
+
+local cmds = cmdsLoader(funcs)
 
 local Players = game:GetService("Players")
 
