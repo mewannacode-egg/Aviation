97c17773b4a42106fde3c8c617812f5459808569
mewannacode-egg <mewannacode@gmail.com>
2026-03-26 21:02:24 +0530

    Update funcs.lua

--- funcs.lua
+++ funcs.lua
@@ -2,17 +2,46 @@ local funcs = {}
 
 local Players = game:GetService("Players")
 
-function funcs:GetPlayer(str)
+--Player targetting
+function funcs:GetPlayers(str, sender)
+	local Players = game:GetService("Players")
 	str = string.lower(str)
+
+	local isRandom = false
+
+	if str == "random/" then
+		isRandom = true
+	end
+
+	if isRandom then
+		local pool = {}
+
+		for _, plr in ipairs(Players:GetPlayers()) do
+			if plr ~= sender then
+				table.insert(pool, plr)
+			end
+		end
+
+		if #pool > 0 then
+			return {pool[math.random(1, #pool)]}
+		end
+
+		return {}
+	end
+
+	local targets = {}
 
 	for _, plr in ipairs(Players:GetPlayers()) do
 		if string.find(string.lower(plr.Name), str, 1, true)
 		or string.find(string.lower(plr.DisplayName), str, 1, true) then
-			return plr
+			table.insert(targets, plr)
 		end
 	end
+
+	return targets
 end
 
+---functions for commands---
 function funcs:Goto(from, to)
 	if not (from.Character and to.Character) then return end
 
