7b8f46b96a0ffe0fab6fbc99bea24be8701250c6
mewannacode-egg <mewannacode@gmail.com>
2026-03-26 18:58:49 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,25 +1,9 @@
+local Players = game:GetService("Players")
+
 local cmds = require(script.Parent.cmds)
 
-local prefix = "-"
-
-local function runCommand(message)
-    if message:sub(1, #prefix) ~= prefix then return end
-
-    local args = {}
-    for word in message:sub(#prefix + 1):gmatch("%S+") do
-        table.insert(args, word)
-    end
-
-    local cmdName = table.remove(args, 1)
-    cmdName = string.lower(cmdName or "")
-
-    local cmd = cmds[cmdName]
-    if cmd then
-        cmd(args)
-    else
-        warn("unknown command:", cmdName)
-    end
-end
-
--- example trigger (you can replace this with your GUI input)
-game:GetService("Players").LocalPlayer.Chatted:Connect(runCommand)
+Players.PlayerAdded:Connect(function(player)
+	player.Chatted:Connect(function(msg)
+		cmds:Execute(player, msg)
+	end)
+end)
